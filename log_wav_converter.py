8e118999eccdeb87f8e83f40d8d62bf3f7e2cf58
cs53 <surumeroid@gmail.com>
2016-06-20 12:43:56 +0900

    body

---
+++ log_wav_converter.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy
+import scipy.io.wavfile
+import matplotlib.pyplot as plt
+
+rate = 44100
+
+input_txt_file_name = 'wave_log3.txt'
+f = open(input_txt_file_name, 'r')
+line = f.readline()
+l=[]
+while line:
+    l.append(line) 
+    line = f.readline()
+wav_data = np.asarray(l, dtype=np.float32)
+f.close
+
+wav_data = (wav_data/np.amax(wav_data))*32767
+wav_data = np.asarray(wav_data, dtype=np.int16)
+output_wav_file_name = "convert.wav"
+scipy.io.wavfile.write(output_wav_file_name,rate,wav_data)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.plot(wav_data)
+output_png_file_name = "convert.png" 
+fig.savefig(output_png_file_name) 
